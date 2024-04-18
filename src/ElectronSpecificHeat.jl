78be27cde77c1b00fec60d8206b31eb8b0716ec0
spagdoon0411 <spandan.suthar@live.com>
2024-04-18 09:54:56 -0700

    add plotting for density of states

--- src/ElectronSpecificHeat.jl
+++ src/ElectronSpecificHeat.jl
@@ -71,7 +71,7 @@ function plot_df_dT(μ)
     fig[1, 1],
     xlabel="Energy (J)",
     ylabel="Temperature (K)",
-    zlabel="f(ϵ, T)"
+    zlabel="df/dt(ϵ, T)"
   )
   surface!(ϵs, Ts, df_dTs)
   return fig
@@ -82,7 +82,32 @@ function plot_df_dT_textbook()
   return plot_df_dT(μ)
 end
 
+function D(N, ϵ, L)
+  return (L^3 / (2π^2)) * (2 * m_e / ħ^2)^(3 / 2) * ϵ^(1 / 2)
+end
+
+N = 1e17
+L = 1
+function plot_density_of_states()
+  ϵ_min = 0
+  ϵ_max = ϵ_F(N, L) * 1.5
+  ϵs = range(ϵ_min, ϵ_max, length=400)
+  Ds = [D(N, ϵ, L) / N for ϵ in ϵs]
+
+  fig = Figure(size=(800, 600))
+  axis = Axis(
+    fig[1, 1],
+    xlabel="Energy ϵ (J)",
+    ylabel="Normalized density of states D(ϵ) / N",
+    limits=(ϵ_min, ϵ_max, D(N, ϵ_min, L) / N, D(N, ϵ_max, L) / N),
+  )
+  lines!(axis, ϵs, Ds)
+  vlines!(axis, [ϵ_F(N, L)], color=:red, linewidth=1.5, linestyle=:dash, label="ϵ_F")
+  return fig
+end
+
 export k_B, ħ, m_e, ϵ_from_book, ϵ_min, ϵ_max, T_min, T_max, f, ϵ_F,
-  plot_fermi_dirac, plot_fermi_dirac_textbook, plot_df_dT, plot_df_dT_textbook
+  plot_fermi_dirac, plot_fermi_dirac_textbook, plot_df_dT, plot_df_dT_textbook,
+  plot_density_of_states, D
 
 end # module ElectronSpecificHeat
