70c4ca6ee168e3f8f5d04897aafd0a7e2d9b6109
spagdoon0411 <spandan.suthar@live.com>
2024-04-18 08:14:25 -0700

    add plotting for df_dT

--- src/ElectronSpecificHeat.jl
+++ src/ElectronSpecificHeat.jl
@@ -3,6 +3,7 @@ module ElectronSpecificHeat
 using PhysicalConstants
 using Unitful
 using GLMakie
+using ForwardDiff
 
 # Constants
 const k_B = ustrip(PhysicalConstants.CODATA2018.k_B)
@@ -22,20 +23,20 @@ T_min = 500
 T_max = 10e4
 
 # Fermi-Dirac distribution
-function fermi_dirac(ϵ, T, μ)
+function f(ϵ, T, μ)
   return 1 / (exp((ϵ - μ) / (k_B * T)) + 1)
 end
 
 function ϵ_F(N, L)
-  return ħ^2 * (3 * π^2 * N / L^3)^(2 / 3) / (2 * m_e)
+  return (ħ^2 / (2 * m_e)) * (3 * π^2 * N / L^3)^(2 / 3)
 end
 
 # Plots the Fermi-Dirac distribution as a function of temperature and energy
 function plot_fermi_dirac(μ)
   ϵs = range(ϵ_min, ϵ_max, length=400)
   Ts = range(T_min, T_max, length=400)
-  f(ϵ, T) = fermi_dirac(ϵ, T, μ)
-  fs = [f(ϵ, T) for ϵ in ϵs, T in Ts]
+  fermi_dirac(ϵ, T) = f(ϵ, T, μ)
+  fs = [fermi_dirac(ϵ, T) for ϵ in ϵs, T in Ts]
 
   fig = Figure(size=(800, 600))
   axis = Axis3(
@@ -49,12 +50,39 @@ function plot_fermi_dirac(μ)
 end
 
 # Plots the Fermi-Dirac distribution using the condition T_F = ϵ_F / k_B = 5e4,
-# as on Kittel 136
+# as on Kittel 136, with the assuption that μ = ϵ_F
 function plot_fermi_dirac_textbook()
-  μ = ϵ_F(5e4 * k_B, 1)
+  μ = 5e4 * k_B
   return plot_fermi_dirac(μ)
 end
 
-export k_B, ħ, m_e, ϵ_from_book, ϵ_min, ϵ_max, T_min, T_max, fermi_dirac, ϵ_F, plot_fermi_dirac, plot_fermi_dirac_textbook
+function df_dT(ϵ, T, μ)
+  return ForwardDiff.derivative(T -> f(ϵ, T, μ), T)
+end
+
+function plot_df_dT(μ)
+  ϵs = range(ϵ_min, ϵ_max, length=400)
+  Ts = range(T_min, T_max, length=400)
+  fermi_dirac_deriv(ϵ, T) = df_dT(ϵ, T, μ)
+  df_dTs = [fermi_dirac_deriv(ϵ, T) for ϵ in ϵs, T in Ts]
+
+  fig = Figure(size=(800, 600))
+  axis = Axis3(
+    fig[1, 1],
+    xlabel="Energy (J)",
+    ylabel="Temperature (K)",
+    zlabel="f(ϵ, T)"
+  )
+  surface!(ϵs, Ts, df_dTs)
+  return fig
+end
+
+function plot_df_dT_textbook()
+  μ = 5e4 * k_B
+  return plot_df_dT(μ)
+end
+
+export k_B, ħ, m_e, ϵ_from_book, ϵ_min, ϵ_max, T_min, T_max, f, ϵ_F,
+  plot_fermi_dirac, plot_fermi_dirac_textbook, plot_df_dT, plot_df_dT_textbook
 
 end # module ElectronSpecificHeat
